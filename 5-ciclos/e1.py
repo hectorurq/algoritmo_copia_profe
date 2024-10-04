dbdc15810787c729aad8f1386a348f60ab60371f
hectorurq <146399640+hectorurq@users.noreply.github.com>
2024-10-04 10:37:54 -0400

    Update e1.py

--- 5-ciclos/e1.py
+++ 5-ciclos/e1.py
@@ -1,1 +1,16 @@
 import typing
+oracion = ["Estoy", "imprimiendo", "una", "lista", "linea", "por", "linea"]
+
+def ejercicio_0_1():
+    for i in (oracion):
+        print(i)
+
+def ejercicio_0_2():
+    aux = 1
+    while aux <= 30:
+        aux_final = aux%5
+        if aux_final == 0:
+            print(aux)
+
+
+        aux += 1
