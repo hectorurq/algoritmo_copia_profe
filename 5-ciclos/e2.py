fc4f2f2b133e199778405cc08fb81929d2756a3b
hectorurq <146399640+hectorurq@users.noreply.github.com>
2024-10-04 10:38:51 -0400

    Update e2.py

--- 5-ciclos/e2.py
+++ 5-ciclos/e2.py
@@ -1,1 +1,36 @@
 import typing
+students = ["Elijah Evans", "Charissa Mueller", "Kirby Park",
+     "Jescie Hill",
+     "Leroy Herring",
+     "Francis Castaneda",
+     "Bree Gregory",
+     "Kermit Stevens",
+     "Jordan Terry",
+     "Paul Trujillo",
+     "Linda Beck",
+     "Dara Mckinney",
+     "Idola Pearson",
+     "Phelan Vazquez",
+     "Cleo Dyer",
+     "Kameko Mccall",
+     "Kitra Tillman",
+     "Oren Miles",
+     "Martin Kerr",
+     "Orlando Noble",
+     "Ferdinand Carr",
+     "Scott Norman",
+     "Autumn Winters",
+     "Phillip Heath",
+     "Ryder Mueller",
+     "Armand Lucas",
+     "Naida Hart",
+     "Deborah Spencer",
+     "Blythe Kidd",
+     "Colleen Keller"]
+
+
+def ejercicio_1_1():
+    for i in students:
+        students.append(i.lower().replace(" ", "_"))
+        
+        print(students)
